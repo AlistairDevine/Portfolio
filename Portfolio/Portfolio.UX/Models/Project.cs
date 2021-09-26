950540f8cd645c96e6869a56be5f83d4f82aed8a
alistairdevine <alistair.devine98@gmail.com>
2021-09-26 21:14:23 +0100

    Loading screen finishes and data pipeline imeplementation

---
+++ Portfolio/Portfolio.UX/Models/Project.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.UX.Models
+{
+    public class Project
+    {
+        public string Full_Name { get; set; }
+        public string Description { get; set; }
+        public string HTML_URL { get; set; }
+        public DateTime Created_At { get; set; }
+        public DateTime Updated_At { get; set; }
+        public string Language { get; set; }
+        public bool Fork { get; set; }
+    }
+}
