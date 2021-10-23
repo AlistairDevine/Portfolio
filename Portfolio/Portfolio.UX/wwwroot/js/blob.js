add09c5d20e9e5555f0bf22794b17ccabd6763bc
alistairdevine <alistair.devine98@gmail.com>
2021-10-23 18:32:17 +0100

    testing environment code implementation

---
+++ Portfolio/Portfolio.UX/wwwroot/js/blob.js
@@ -0,0 +1,14 @@
+﻿$(window).on("scroll", function () {
+    if ($(window).scrollTop() > 15) {
+        $(".container").addClass("on-scroll");
+    } else {
+        $(".container").removeClass("on-scroll");
+    }
+});
+
+const tween = KUTE.fromTo(
+    '#blobOne',
+    { path: '#blobOne' },
+    { path: '#blobTwo' },
+    { repeat: 999, duration: 3000, yoyo: true }
+).start();
